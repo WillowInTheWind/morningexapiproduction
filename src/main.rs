45f8af3ff9fc123b19dead9589cce595b43c8fc6
WillowintheWind <wayland.chase@gmail.com>
2024-04-20 13:38:16 -0500

    dockerfile

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,36 @@ use sqlx::postgres::PgPoolOptions;
 #[tokio::main]
 async fn main()
 {
+        println!("->> Server Init Beginning");
         dotenv::dotenv().ok();
     //Init environment variables and tracing
         tracing_subscriber::fmt::init();
-        let database_url =env::var("DATABASE_URL").expect("DATABASE_URL must set");
+        let oauth_client = config::oauth_client().unwrap();
+
         let environment_variables = config::initialize_environment_variable()
             .await;
+        let database_url =env::var("DATABASE_URL").expect("DATABASE_URL must set");
     //Init App State
+        println!("->> Enviorment variables initilized");
+
         let pool = PgPoolOptions::new()
             .max_connections(5)
             .connect(&database_url)
             .await
             .expect("could not connect");
+        println!("->> Successful connection to postgres database");
 
-        let oauth_client = config::oauth_client().unwrap();
-        let client = reqwest::Client::new();
+        let client = reqwest::Client::builder().use_rustls_tls().build().unwrap()
+            ;
+        println!("->> HTTP client initialized");
         let app_state: types::state::AppState  = types::state::AppState {
             dbreference: pool,
             oauth_client,
             reqwest_client: client,
         };
-        println!("->> Successful connection to postgres database");
     //Init App routes
         let app_router = routes::router(app_state);
+        println!("->> Router initialized");
     //Launch Server
         let server_adress: String = format!("{}:{}", environment_variables.address, environment_variables.port);
         let listener = tokio::net::TcpListener::bind(
